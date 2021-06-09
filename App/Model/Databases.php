fbe115655f76580f58b81d3063cdb3d206c7282c
tienpham2009 <tiendx2009@gmail.com>
2021-06-09 14:52:33 +0700

    master

---
+++ App/Model/Databases.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Model;
+
+class Databases
+{
+    protected $dsn;
+    protected $user;
+    protected $password;
+
+    public function __construct()
+    {
+        $this->dsn = "mysql:host=localhost;dbname=mange_motel";
+        $this->user = "root";
+        $this->password = "200997";
+    }
+
+    public function connect()
+    {
+        try {
+            return new PDO($this->dsn,$this->user,$this->password);
+        }catch (PDOException $PDOException){
+            echo $PDOException->getMessage();
+            die();
+        }
+    }
+}
---
+++ App/Model/Model_Interface.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Model;
+
+interface Model_Interface
+{
+    public function getAll();
+    public function getById();
+}
