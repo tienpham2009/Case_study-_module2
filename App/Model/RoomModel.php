eb9550013c5a59bce765a9ff0b1032e26c32f4df
henshim <hieume113@gmail.com>
2021-06-09 17:24:09 +0700

    hieu4

--- App/Model/RoomModel.php
+++ App/Model/RoomModel.php
@@ -39,4 +39,12 @@ class RoomModel extends Models implements Model_Interface
 
         return $stmt->execute();
     }
+
+    function delete($id)
+    {
+        $sql = 'delete from room where Id=?';
+        $stmt = $this->connect->prepare($sql);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+    }
 }
