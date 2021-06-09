3e7fd62bc471720528bde669f882c5309d81fd0a
henshim <hieume113@gmail.com>
2021-06-09 16:36:28 +0700

    hieu

--- App/Model/RoomModel.php
+++ App/Model/RoomModel.php
@@ -4,8 +4,23 @@
 namespace App\Model;
 
 
-class RoomModel extends Models
+class RoomModel extends Models implements Model_Interface
 {
+    public function getAll()
+    {
+        $sql = 'select *from room';
+        $stmt = $this->connect->query($sql);
+        $stmt->fetchAll();
+    }
+
+    public function getById($id)
+    {
+        $sql = 'select *from room where Id=?';
+        $stmt = $this->connect->prepare($sql);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+    }
+
     public function add($object)
     {
         $sql = "INSERT INTO room( name,description,image,unit_price,status,category,check_in,check_out) 
@@ -13,14 +28,14 @@ class RoomModel extends Models
 
         $stmt = $this->connect->prepare($sql);
 
-        $stmt->bindParam(":name" , $object->name);
-        $stmt->bindParam(":description" , $object->name);
-        $stmt->bindParam(":image" , $object->name);
-        $stmt->bindParam(":unit_price" , $object->name);
-        $stmt->bindParam(":status" , $object->name);
-        $stmt->bindParam(":category" , $object->name);
-        $stmt->bindParam(":check_in" , $object->name);
-        $stmt->bindParam(":check_out" , $object->name);
+        $stmt->bindParam(":name", $object->name);
+        $stmt->bindParam(":description", $object->name);
+        $stmt->bindParam(":image", $object->name);
+        $stmt->bindParam(":unit_price", $object->name);
+        $stmt->bindParam(":status", $object->name);
+        $stmt->bindParam(":category", $object->name);
+        $stmt->bindParam(":check_in", $object->name);
+        $stmt->bindParam(":check_out", $object->name);
 
         return $stmt->execute();
     }
