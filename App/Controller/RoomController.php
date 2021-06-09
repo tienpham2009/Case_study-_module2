6fa366ad656585b110cedc02556685d5a31a3699
tienpham2009 <tiendx2009@gmail.com>
2021-06-09 16:28:16 +0700

    add

---
+++ App/Controller/RoomController.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Controller;
+
+use App\Model\RoomModel;
+
+class RoomController
+{
+    public $roomDB;
+
+    public function __construct()
+    {
+        $this->roomDB = new RoomModel();
+    }
+
+    public function getImage()
+    {
+        $fileError = [];
+        $target_dir = "Public/Image/";
+        $target_name = basename($_FILES["fileToUpload"]["name"]);
+
+        $target_file = $target_dir. $target_name;
+
+        if ($_FILES["fileToUpload"]["size"] > 5000000){
+            $fileError["fileUpload"] = "Chỉ được upload file dưới 5MB";
+        }
+
+        $file_type = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+
+        $file_type_allow = ["jpg","png","jpeg","gif"];
+
+        if (!in_array(strtolower($file_type), $file_type_allow)){
+            $fileError["fileUpload"] = "Chỉ được upload file ảnh";
+        }
+
+        if (file_exists($target_file)){
+            $fileError["fileUpload"] = "File đã tồn tại trên hệ thống";
+        }
+
+        if (empty($fileError)){
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        }
+        return $target_name;
+    }
+
+    public function getDataRoom()
+    {
+        $name = $_POST["name"];
+        $description = $_POST["description"];
+        $unit_price = $_POST["unit_price"];
+        $status = $_POST["status"];
+        $category = $_POST["category"];
+        $check_in = $_POST["check_in"];
+        $check_out = $_POST["check_out"];
+        $image = $this->getImage();
+
+        $data = [
+            "name"
+            "name"
+            "name"
+            "name"
+            "name"
+            "name"
+            "name"
+        ]
+    }
+
+    public function add()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "GET"){
+            include "View/room/add.php";
+        }else{
+
+        }
+    }
+}
--- App/Model/Databases.php
+++ App/Model/Databases.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Model;
 
+use PDO;
+use PDOException;
+
 class Databases
 {
     protected $dsn;
