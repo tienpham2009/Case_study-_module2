01fd0f652c85f4b85cbbf28e9054d83b4a60a75b
henshim <hieume113@gmail.com>
2021-06-10 16:59:06 +0700

    hieu

--- View/room/list.php
+++ View/room/list.php
@@ -1,36 +1,43 @@
 <?php foreach ($rooms as $key => $room): ?>
     <!--<article class="col-12 col-sm-9 mt-2">-->
     <!--    <div class="col-12 col-sm-12 row mb-2">-->
+    <style>
+        .new {
+            background-color: yellow
+        }
+    </style>
     <div class="col-sm-4">
-        <div class="card text-center">
-            <div class="card-header">
-                <?php echo $key + 1 ?>
-            </div>
-            <div class="card-header">
-                <?php echo $room->name ?>
-            </div>
-            <div class="card-header">
-                <?php echo $room->description ?>
-            </div>
-            <div class="card-header">
-                <?php echo $room->unit_price ?>
-            </div>
-            <div class="card-header">
-                <?php echo $room->status ?>
-            </div>
-            <div class="card-header">
-                <?php echo $room->cateName ?>
-            </div>
-            <div class="card-header" >
-                <a href="index.php?page=room&action=update&id=<?php echo $room->id ?>"
-                        type="button" class="btn btn-primary">Update
-                </a>
-                <a href="index.php?page=room&action=delete&id=<?php echo $room->id ?>"
-                        type="button" class="btn btn-danger">Delete
-                </a>
-                <a href="index.php?page=room&action=check_in&id=<?php echo $room->id ?>"
-                        type="submit" class="btn btn-info">Check in
-                </a>
+        <div class="card text-center ">
+            <div class="<?= $room->status !== 'empty' ?? 'new' ?>">
+                <div class="card-header">
+                    <?php echo $key + 1 ?>
+                </div>
+                <div class="card-header">
+                    <?php echo $room->name ?>
+                </div>
+                <div class="card-body">
+                    <?php echo $room->description ?>
+                </div>
+                <div class="card-body">
+                    <?php echo $room->unit_price ?>
+                </div>
+                <div class="card-body">
+                    <?php echo $room->status ?>
+                </div>
+                <div class="card-body">
+                    <?php echo $room->cateName ?>
+                </div>
+                <div class="card-header">
+                    <a href="index.php?page=room&action=update&id=<?php echo $room->id ?>"
+                       type="button" class="btn btn-primary">Update
+                    </a>
+                    <a href="index.php?page=room&action=delete&id=<?php echo $room->id ?>"
+                       type="button" class="btn btn-danger">Delete
+                    </a>
+                    <a href="index.php?page=room&action=check_in&id=<?php echo $room->id ?>"
+                       type="submit" class="btn btn-info">Check in
+                    </a>
+                </div>
             </div>
         </div>
     </div>
