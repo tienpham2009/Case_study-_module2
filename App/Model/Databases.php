5be292342841b889346316dcc2eefbfcba5fb6a5
tienpham2009 <tiendx2009@gmail.com>
2021-06-11 09:59:44 +0700

    tien

--- App/Model/Databases.php
+++ App/Model/Databases.php
@@ -12,10 +12,9 @@ class Databases
 
     public function __construct()
     {
-        $this->dsn = "mysql:host=localhost;dbname=mange_motel";
+        $this->dsn = "mysql:host=localhost;dbname=manage_motel";
         $this->user = "root";
         $this->password = "200997";
-
     }
 
     public function connect()
